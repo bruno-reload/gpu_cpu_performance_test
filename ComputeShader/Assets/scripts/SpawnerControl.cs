aba8dcc5da9fcf1a128ee8a1ea6156045d5d2586
karina gomes de siqueira <81597895+karinanani@users.noreply.github.com>
2022-07-13 20:46:52 -0300

    controle de gpu e cpu no update

--- ComputeShader/Assets/scripts/SpawnerControl.cs
+++ ComputeShader/Assets/scripts/SpawnerControl.cs
@@ -54,4 +54,63 @@ public class SpawnerControl : MonoBehaviour
         computeShader.SetFloat("floor", floor.GetComponent<Transform>().position.y);
         computeShader.SetFloat("height", transform.position.y);
     }
+
+    void Update()
+    {
+        count = Time.realtimeSinceStartup;
+        if (gpu)
+        {
+            time += Time.deltaTime;
+            computeShader.SetFloat("tf", time);
+            computeShader.Dispatch(0, spheres.Length / 10, 1, 1);
+
+            computeBuffer.GetData(spheres);
+
+            if (avaliableSpeedup)
+            {
+                count = Time.realtimeSinceStartup - count;
+                Debug.Log("speedup gpb: " + count);
+                avaliableSpeedup = false;
+            }
+        }
+        if (cpu)
+        {
+            time += Time.deltaTime;
+            delta = time - delta;
+            for (int i = 0; i < spheres.Length; i++)
+            {
+                if (!finished[i])
+                {
+                    float f = spheres[i].weight * (spheres[i].speed / time);
+                    float speed = (f / spheres[i].weight) * delta;
+                    Vector3 p = this.gobjSpheres[i].transform.position;
+                    if (p.y - spheres[i].speed * delta - spheres[i].radius > floor.transform.position.y)
+                    {
+                        this.spheres[i].position += new Vector3(0, speed * delta * spheres[i].direction.y, 0);
+                        this.spheres[i].color = new Vector3(1, 1, 1);
+                    }
+                    else
+                    {
+                        this.spheres[i].position = new Vector3(p.x, floor.transform.position.y + spheres[i].radius, p.z);
+                        spheres[i].color = new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+                        finished[i] = true;
+                    }
+                }
+            }
+            if (avaliableSpeedup)
+            {
+                count = Time.realtimeSinceStartup - count;
+                Debug.Log("speedup cpu: " + count);
+                avaliableSpeedup = false;
+            }
+        }
+        for (int i = 0; i < spheres.Length; i++)
+        {
+            Vector3 pos = this.gobjSpheres[i].transform.position;
+            this.gobjSpheres[i].transform.position = new Vector3(pos.x, spheres[i].position.y, pos.z);
+            Color c = new Color(spheres[i].color.x, spheres[i].color.y, spheres[i].color.z, 1);
+
+            this.gobjSpheres[i].GetComponent<Renderer>().material.color = c;
+        }
+    }
 }
