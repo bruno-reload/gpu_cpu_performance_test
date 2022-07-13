3697c673ba9d745f4c61a7a284642e0c5a7ee9d8
paola390 <75397487+paola390@users.noreply.github.com>
2022-07-13 20:27:47 -0300

    vetores iniciados

--- ComputeShader/Assets/scripts/SpawnerControl.cs
+++ ComputeShader/Assets/scripts/SpawnerControl.cs
@@ -3,5 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnerControl : MonoBehaviour
-{
+{ 
+    void Start()
+    {
+        this.spheres = new Sphere[ObjCount * ObjCount];
+        this.gobjSpheres = new GameObject[ObjCount * ObjCount];
+        this.finished = new bool[ObjCount * ObjCount];
+        for (int i = 0, k = 0; i < ObjCount; i++)
+        {
+            float offsetX = (-ObjCount / 2 + i);
+            for (int j = 0; j < ObjCount; j++, k++)
+            {
+                float offsetZ = (-ObjCount / 2 + j);
+                gobjSpheres[k] = Instantiate(obj);
+                gobjSpheres[k].transform.position = new Vector3(offsetX * distance + .5f, transform.position.y, offsetZ * distance + .5f);
+                spheres[k].position = gobjSpheres[i].transform.position;
+                spheres[k].radius = .5f;
+                spheres[k].direction = Vector3.down;
+                spheres[k].speed = Random.Range(MinSpeed, MaxSpeed);
+                spheres[k].weight = Random.Range(MinWeight, MaxWeight);
+                finished[k] = false;
+
+            }
+        }
+        int totalSize = sizeof(float) * 3 + sizeof(float) * 3 + sizeof(float) + sizeof(float) * 3 + sizeof(float) + sizeof(float);
+        computeBuffer = new ComputeBuffer(spheres.Length, totalSize);
+        computeBuffer.SetData(spheres);
+
+
+        computeShader.SetBuffer(0, "objs", computeBuffer);
+        computeShader.SetFloat("floor", floor.GetComponent<Transform>().position.y);
+        computeShader.SetFloat("height", transform.position.y);
+    }
 }
